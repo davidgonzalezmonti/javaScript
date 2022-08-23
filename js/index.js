c098b711393b407968f30e40bc10712564cbbc1e
David Gonzalez Monti <gonzalezmontidavid@gamil.com>
2022-08-23 17:27:48 -0300

    nueva rama

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
-let entrada = prompt("¡BIENVENIDO!" + "\n" + "vamos a formar un equipo de futbol 5, ingrese el nombre de su equipo");
-console.log("Nombre del equipo: " + entrada)
+ let entrada = prompt("¡BIENVENIDO!" + "\n" + "vamos a formar un equipo de futbol 5, ingrese el nombre de su equipo");
+ console.log("Nombre del equipo: " + entrada)
 
-for (let i = 1; i <= 5; i++) {
-    var nombres = prompt("Ingrese el nombre de uno de los jugadores");
+ for (let i = 1; i <= 5; i++) {
+     var nombres = prompt("Ingrese el nombre de uno de los jugadores");
 
-    if (!isNaN(nombres)) {
-        alert("Ingreso un nombre invalido, vuelve a cargar la pagina");
-        break;
-    }
+     if (!isNaN(nombres)) {
+         alert("Ingreso un nombre invalido, vuelve a cargar la pagina");
+         break;
+     }
 
-    alert("Bienvenido " + nombres);
-    console.log("Jugador Nº " + i + ": " + nombres);
-}
+     alert("Bienvenido " + nombres);
+     console.log("Jugador Nº " + i + ": " + nombres);
+ }
 
-if (isNaN(nombres)) {
+ if (isNaN(nombres)) {
     alert('¡Felicitaciones, has formado un equipo de Futbol 5!');
 
-}
+ }
+

639a1ecb49b7a3ffa58db7cf98c7d08cd7445f75
David Gonzalez Monti <gonzalezmontidavid@gamil.com>
2022-08-26 20:23:14 -0300

    Entrega simulador interactivo

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-let entrada = Number (prompt("Cuantos Stickers vas a llevar?"))
-let stickers = 100
-let resultado = entrada * stickers
+let entrada = Number (prompt("Cuantos Stickers vas a llevar?"));
+let stickers = 100;
+let resultado = entrada * stickers;
 
 
-const THIRTY = 30
-const TWENTY = 20
-const FIFTEEN = 15
-let oneHundred = 100
+const THIRTY = 30;
+const TWENTY = 20;
+const FIFTEEN = 15;
+let oneHundred = 100;
 
 
 let cuotas = (multiplicacion, division, cantCuotas) => {
-    var porcentaje = resultado * multiplicacion / division
+    var porcentaje = resultado * multiplicacion / division;
             console.log("Intereses: " + porcentaje);
             var precioAPagar = (porcentaje + resultado) / cantCuotas;
             var precioTotal = resultado + porcentaje;
             console.log("Precio total: " + precioTotal);
             console.log("12 cuotas de: " + precioAPagar);
-            console.log("Gracias por su compra")
+            console.log("Gracias por su compra");
             return;
 }
 
@@ -27,20 +27,20 @@ function mensaje (mensajeUno, mensajeDos){
 
 function compraStickers() {
 do {
-    valor = Number (prompt("En cuantas cuotas lo queres pagar? \n 12 cuotas con recargo del 30% \n 6 cuotas con recargo del 20% \n 3 cuotas con recargo del 10% \n 1 cuota sin interes \n 0. Salir"))
+    valor = Number (prompt("En cuantas cuotas lo queres pagar? \n 12 cuotas con recargo del 30% \n 6 cuotas con recargo del 20% \n 3 cuotas con recargo del 10% \n 1 cuota sin interes \n 0. Salir"));
     switch (valor) {
         case 12:
-           return cuotas (THIRTY,oneHundred,12)
+           return cuotas (THIRTY,oneHundred,12);
 
         case 6:
-            return cuotas (TWENTY,oneHundred,6)
+            return cuotas (TWENTY,oneHundred,6);
 
             case 3:
-                return cuotas (FIFTEEN,oneHundred,3)
+                return cuotas (FIFTEEN,oneHundred,3);
 
             case 1:
             mensaje ("Total a pagar: ", resultado);
-            console.log("Gracias por su compra")
+            console.log("Gracias por su compra");
             return;
 
             case 0:
@@ -51,7 +51,7 @@ do {
             console.log("seleccione una opcion correcta");
             break;
     }
-} while (valor !== 0 || valor === null )
+} while (valor !== 0 || valor === null );
 
 }
 

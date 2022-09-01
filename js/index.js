82302381589cde37d17eff0812aa5f1ada246d46
David Gonzalez Monti <gonzalezmontidavid@gamil.com>
2022-09-01 18:25:00 -0300

    incorporandoArrays

--- js/index.js
+++ js/index.js
@@ -1,59 +1,52 @@
-let entrada = Number (prompt("Cuantos Stickers vas a llevar?"));
-let stickers = 100;
-let resultado = entrada * stickers;
+const arrayDeStickers = [];
 
+const luffy = { nombre: "Luffy", tamaño: "10x10", precio: 150 }
+const goku = { nombre: "Goku", tamaño: "8x8", precio: 130 }
+const vegeta = { nombre: "Vegeta", tamaño: "6x6", precio: 110 }
 
-const THIRTY = 30;
-const TWENTY = 20;
-const FIFTEEN = 15;
-let oneHundred = 100;
+console.log(luffy, goku, vegeta);
 
-
-let cuotas = (multiplicacion, division, cantCuotas) => {
-    var porcentaje = resultado * multiplicacion / division;
-            console.log("Intereses: " + porcentaje);
-            var precioAPagar = (porcentaje + resultado) / cantCuotas;
-            var precioTotal = resultado + porcentaje;
-            console.log("Precio total: " + precioTotal);
-            console.log("12 cuotas de: " + precioAPagar);
-            console.log("Gracias por su compra");
-            return;
-}
-
-function mensaje (mensajeUno, mensajeDos){
-    console.log (mensajeUno + mensajeDos);
-}
-
-
-function compraStickers() {
-do {
-    valor = Number (prompt("En cuantas cuotas lo queres pagar? \n 12 cuotas con recargo del 30% \n 6 cuotas con recargo del 20% \n 3 cuotas con recargo del 10% \n 1 cuota sin interes \n 0. Salir"));
-    switch (valor) {
-        case 12:
-           return cuotas (THIRTY,oneHundred,12);
-
-        case 6:
-            return cuotas (TWENTY,oneHundred,6);
-
-            case 3:
-                return cuotas (FIFTEEN,oneHundred,3);
-
-            case 1:
-            mensaje ("Total a pagar: ", resultado);
-            console.log("Gracias por su compra");
-            return;
-
-            case 0:
-                console.log("Gracias por su visita");
-                break;
-    
-        default:
-            console.log("seleccione una opcion correcta");
-            break;
+function precioAPagar() {
+    console.log("DETALLE DE SU COMPRA: \n");
+    console.log(arrayDeStickers);
+    let precioTotal = 0
+    for (let i = 0; i < arrayDeStickers.length; i++) {
+        precioTotal = precioTotal + arrayDeStickers[i].precio
     }
-} while (valor !== 0 || valor === null );
-
+    console.log("Total a pagar: $" + precioTotal);
+    alert("Gracias por su compra ❤️".toUpperCase());
+    
 }
 
-mensaje ("Precio de lista: ", resultado);
-compraStickers ();
+function compraDeStickers () {
+do {
+    opcion = Number(prompt("Que stickers quieres llevar? \n 4. Luffy \n 3. Goku \n 2. Vegeta \n 1. Eliminar ultimo producto \n 0. Finalizar la compra"))
+    switch (opcion) {
+        case 4:
+            arrayDeStickers.push(luffy);
+
+            break
+        case 3:
+            arrayDeStickers.push(goku);
+
+            break
+
+        case 2:
+            arrayDeStickers.push(vegeta);
+
+            break
+        case 1:
+            arrayDeStickers.pop();
+            break
+
+        case 0:
+            precioAPagar();
+            break
+        default:
+            alert("Seleccione una opcion correcta");
+            break
+    }
+} while (opcion !== 0 || opcion === null)
+}
+
+compraDeStickers();
